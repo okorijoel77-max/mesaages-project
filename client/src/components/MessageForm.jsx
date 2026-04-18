29dd67c142a8d265de87f559c020406d887cde81
Okori Jnr <okorijoel77@gmail.com>
2026-04-19 00:06:05 +0100

    fix MessageForm export properly

--- client/src/components/MessageForm.jsx
+++ client/src/components/MessageForm.jsx
@@ -1,21 +1,50 @@
-<form onSubmit={handleSubmit}>
-  <input
-    placeholder="Name"
-    value={name}
-    onChange={(e) => setName(e.target.value)}
-  />
+import { useState } from "react";
+import { addMessage } from "../api";
 
-  <input
-    placeholder="Email"
-    value={email}
-    onChange={(e) => setEmail(e.target.value)}
-  />
+export default function MessageForm({ onAdd }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  <input
-    placeholder="Message"
-    value={message}
-    onChange={(e) => setMessage(e.target.value)}
-  />
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  <button type="submit">Send</button>
-</form>
+    const newMessage = { name, email, message };
+
+    try {
+      await addMessage(newMessage);
+      onAdd();
+
+      // clear inputs
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+
+      <button type="submit">Send</button>
+    </form>
+  );
+}
