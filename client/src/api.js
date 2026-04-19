7a6f7c2e52249a1ef4a067b6e8e1e942dae059aa
Okori Jnr <okorijoel77@gmail.com>
2026-04-19 08:27:19 +0100

    add debug logs to api

--- client/src/api.js
+++ client/src/api.js
@@ -2,21 +2,45 @@ const BASE_URL = "https://messages-backend-p1nt.onrender.com";
 
 // GET all messages
 export const getMessages = async () => {
-  const res = await fetch(`${BASE_URL}/api/messages`);
-  return res.json();
+  try {
+    const res = await fetch(`${BASE_URL}/api/messages`);
+
+    console.log("GET STATUS:", res.status);
+
+    const data = await res.json();
+    console.log("GET DATA:", data);
+
+    return data;
+  } catch (err) {
+    alert("Failed to load messages");
+    console.log(err);
+    return [];
+  }
 };
 
 // POST new message
 export const addMessage = async (newMessage) => {
-  const res = await fetch(`${BASE_URL}/api/messages`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(newMessage)
-  });
+  try {
+    console.log("SENDING:", newMessage);
 
-  return res.json();
+    const res = await fetch(`${BASE_URL}/api/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newMessage)
+    });
+
+    console.log("POST STATUS:", res.status);
+
+    const data = await res.json();
+    console.log("POST RESPONSE:", data);
+
+    return data;
+  } catch (err) {
+    alert("Failed to send message");
+    console.log(err);
+  }
 };
 
 // DELETE message
