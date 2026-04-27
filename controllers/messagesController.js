ea65c3a4ab3947cfa879414a2ed9b8c75889e371
Okori Jnr <okorijoel77@gmail.com>
2026-04-27 21:46:47 +0100

    fix controller + debug

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,59 +1,22 @@
-// controllers/messagesController.js
-
-console.log("NEW CONTROLLER LOADED");
-
-const db = require("../services/dbService");
-
-const getAllMessages = (req, res) => {
-  const messages = db.getAllMessages();
-  res.json(messages);
-};
-
 const createMessage = (req, res) => {
-  console.log("BODY:", req.body); // 👈 ADD THIS
+  console.log("BODY RECEIVED:", req.body);
 
-  const { name, phone, pickup, dropoff, message } = req.body;
+  const { name, email, message, phone, pickup, dropoff } = req.body;
 
   try {
-    const result = db.addMessage(name, phone, pickup, dropoff,  message);
+    const result = db.addMessage(name, email, message);
 
-    const newMessage = {
+    res.json({
       id: result.lastInsertRowid,
       name,
+      email,
+      message,
       phone,
       pickup,
-      dropoff,
-      message,
-      created_at: new Date().toLocaleString()
-    };
-
-    res.json(newMessage);
+      dropoff
+    });
   } catch (err) {
-    console.error(err);
+    console.log("ERROR:", err);
     res.status(500).json({ error: "Failed to add message" });
   }
-};
-
-const updateMessage = (req, res) => {
-  const id = req.params.id;
-  const { text } = req.body;
-
-  db.updateMessage(id, text);
-
-  res.json({ success: true });
-};
-
-const deleteMessage = (req, res) => {
-  const id = req.params.id;
-
-  db.deleteMessage(id);
-
-  res.json({ success: true });
-};
-
-module.exports = {
-  getAllMessages,
-  createMessage,
-  updateMessage,
-  deleteMessage
 };
